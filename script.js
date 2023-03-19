adfc1c00beaedf420efe7dd114f26cf0792a0551
derique96 <fowlkes.derique@gmail.com>
2023-03-19 17:08:27 -0400

    Working on questions

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-var timerEl = document.getElementById('time');
-var start = document.getElementById('start');
-var previous = document.getElementById('previous');
-var next = document.getElementById('next');
+var timerEl = document.getElementById("time");
+var start = document.getElementById("start");
+var previous = document.getElementById("previous");
+var next = document.getElementById("next");
 var timeLeft = 60;
-
-
-function timer(){
-    var countDown = setInterval(function(){
-        timerEl.textContent = "Time: " + timeLeft;
-        timeLeft--
-        if (timeLeft === 0){
-            clearInterval(countDown);
-            timerEl.textContent=""
-        }
-
+var questionsArray = [
+    {
+      question: "What is an Object",
+      answers: ["A Method", "A group of properties"],
+    },
+    {
+      question: "Which is not a primitive data type?",
+      answers: ["Boolean", "String","Conditional","Undefined"]
     }
-    , 1000);
-    
+  ];
+console.log(questionsArray);
+
+function timer() {
+  var countDown = setInterval(function () {
+    timerEl.textContent = "Time: " + timeLeft + " seconds";
+    timeLeft--;
+    if (timeLeft === 0) {
+      clearInterval(countDown);
+      timerEl.textContent = "";
+    }
+  }, 1000);
 }
-start.addEventListener('click', timer);
+
+start.addEventListener("click", timer);
+document.getElementById("questions").innerHTML = questionsArray
